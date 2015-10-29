cdb993757c0473815111f9847b007ec82b18934e
Laszlo Hasznos <hasznos.laszlo@gmail.com>
2015-10-29 10:56:23 -0700

    avoiding mass assignment, whitelist that lets params flow to the server

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,8 +10,14 @@ class QuestionsController < ApplicationController
 
   def create
     # create will save to the database
-    Question.create title: params[:question][:title],
-                    body: params[:question][:body]})
+    # Question.create title: params[:question][:title],
+    #                 body: params[:question][:body]})
+
+    # this is called Mass assignment
+    # without the whitelist, hackers could chaneg other other attributes of the model
+    # happened to github, people hacked others ssh keys
+    question_params = params.require(:question).permit([:title, :body])
+    Question.create(question_params)
     render text: "Inside Questions Create: #{params[:question]}"
   end
 
